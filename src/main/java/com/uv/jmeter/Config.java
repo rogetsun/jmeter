ca44cbb44dc194bf650aba3c82511a1a2b62e70f
songyw-imac <songyw163@163.com>
2021-06-26 18:09:19 +0800

    commit

--- src/main/java/com/uv/jmeter/Config.java
+++ src/main/java/com/uv/jmeter/Config.java
@@ -6,7 +6,35 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties(prefix = "uv", ignoreInvalidFields = true)
 public class Config {
+
+    /**
+     * 是否计算请求数
+     */
     private boolean isCalculate;
+    /**
+     * 延迟时长
+     */
+    private long sleepSeconds;
+    /**
+     * 触发延迟概率 %
+     */
+    private int delayPercent;
+
+    public int getDelayPercent() {
+        return delayPercent;
+    }
+
+    public void setDelayPercent(int delayPercent) {
+        this.delayPercent = delayPercent;
+    }
+
+    public long getSleepSeconds() {
+        return sleepSeconds;
+    }
+
+    public void setSleepSeconds(long sleepSeconds) {
+        this.sleepSeconds = sleepSeconds;
+    }
 
     public boolean isCalculate() {
         return isCalculate;
--- src/main/java/com/uv/jmeter/controller/DefaultController.java
+++ src/main/java/com/uv/jmeter/controller/DefaultController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -21,20 +23,25 @@ public class DefaultController {
     @Resource
     private UVListener uvListener;
 
+    private final Random random = new Random();
+
     @RequestMapping(value = "/jmeter", method = {RequestMethod.GET, RequestMethod.POST})
     public String jmeterTest(@RequestParam(value = "id", required = false) String id, @RequestParam(value = "name", required = false) String name, HttpServletRequest request) {
         String host = request.getRemoteAddr();
-        log.error("error：id={}, name={}", id, name);
-        log.info("host:{},id={},name={}", host, id, name);
-        log.debug("debug：id={}, name={}", id, name);
-        log.trace("trace：id={}, name={}", id, name);
+        log.debug("error：id={}, name={}", id, name);
+        log.debug("host:{},id={},name={}", host, id, name);
         try {
+            if (random.nextInt(100) < config.getDelayPercent() && config.getSleepSeconds() > 0) {
+                log.debug("sleep {}", config.getSleepSeconds());
+                TimeUnit.SECONDS.sleep(config.getSleepSeconds());
+            }
             if (config.isCalculate()) {
                 calculator.add(id);
             }
         } catch (Exception e) {
             log.error("添加失败，" + id, e);
         }
+        log.debug("/jmeter response over!");
         return "jmeter test";
 
     }
